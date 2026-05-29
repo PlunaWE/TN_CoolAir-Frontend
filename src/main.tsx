20a200cdbd0208001f79188f1272953d925a4ccf
PlunaWE <pedro.luna@wienenergie.at>
2026-05-29 12:25:58 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
-import { initPiwikPro } from "./lib/piwik";
+import { initPiwikPro, loadConsentState, pushConsentState } from "./lib/piwik";
 
 const queryClient = new QueryClient();
 
 initPiwikPro();
+
+const storedConsent = loadConsentState();
+if (storedConsent) {
+  pushConsentState(storedConsent);
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
